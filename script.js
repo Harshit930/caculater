9ca8e63f73d1df96bb5e6a20334344cc67d3a41d
Harshit930 <harshitsingh1930@gmail.com>
2026-01-20 11:46:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+let firstValue = '';
+let lastValue = '';
+let operation = '';
+
+function appendValue(num) {
+    let dis = document.getElementById('display');
+
+    if (operation === '') {
+        firstValue += num;
+        dis.value = firstValue;
+    } else {
+        lastValue += num;
+        dis.value = firstValue + operation + lastValue;
+    }
+}
+
+function appendDotValue() {
+    let dis = document.getElementById('display');
+
+    if (operation === '') {
+        if (!firstValue.includes('.')) {
+            firstValue += '.';
+            dis.value = firstValue;
+        }
+    } else {
+        if (!lastValue.includes('.')) {
+            lastValue += '.';
+            dis.value = firstValue + operation + lastValue;
+        }
+    }
+}
+
+function appendOperation(opr) {
+    if (firstValue === '') return;
+
+    operation = opr;
+    document.getElementById('display').value = firstValue + operation;
+}
+
+function calculate() {
+    if (firstValue === '' || lastValue === '') return;
+
+    let num1 = parseFloat(firstValue);
+    let num2 = parseFloat(lastValue);
+    let result = 0;
+
+    switch (operation) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': result = num1 / num2; break;
+    }
+
+    document.getElementById('display').value = result;
+
+    firstValue = result.toSt
+}
